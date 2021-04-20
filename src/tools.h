8b83e522dd9a3145a2ce191ce03c2bd8ae81ae85
Ying Li <liying2905@gmail.com>
2021-04-20 12:09:30 -0400

    Init commit

---
+++ src/tools.h
@@ -0,0 +1,37 @@
+#ifndef TOOLS_H_
+#define TOOLS_H_
+
+#include <vector>
+#include "Eigen/Dense"
+
+class Tools {
+ public:
+  /**
+   * Constructor.
+   */
+  Tools();
+
+  /**
+   * Destructor.
+   */
+  virtual ~Tools();
+
+  /**
+   * A helper method to calculate RMSE.
+   */
+  Eigen::VectorXd CalculateRMSE(const std::vector<Eigen::VectorXd> &estimations, 
+                                const std::vector<Eigen::VectorXd> &ground_truth);
+
+  /**
+   * A helper method to calculate Jacobians.
+   */
+  Eigen::MatrixXd CalculateJacobian(const Eigen::VectorXd& x_state);
+  
+  /**
+   * A normalizer to convert a phi of any angle to the value of range [-pi, pi].
+   */
+  float normalize(float phi);
+
+};
+
+#endif  // TOOLS_H_
